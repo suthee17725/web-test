df846f1beaff5f42b2bb49bf5e4f1be92b1cd017
suthee17725 <suthee.namsiri@gmail.com>
2023-06-13 20:34:20 +0700

    product

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,48 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SidebarContext } from "../contexts/SidebarContext";
+import { CartContext } from "../contexts/CartContext";
 
 import { BsBag } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import Logo from "../img/logo.svg";
 
 const Header = () => {
+  // header state
+  const [isActive, setIsActive] = useState(false);
   const { isOpen, setIsOpen } = useContext(SidebarContext);
+  const { itemAmount } = useContext(CartContext);
+  //event listener
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 60 ? setIsActive(true) : setIsActive(false);
+    });
+  });
+
   return (
-    <div>
-      <div>Header</div>
-      <div onClick={() => setIsOpen(!isOpen)} className="cursor-pointer">
-        <BsBag className="text-2xl" />
+    <header
+      className={`${
+        isActive ? "bg-white py-4 shadow-md" : "bg-none py-6"
+      } fixed w-full z-10 transition-all`}
+    >
+      <div className="container mx-auto  flex items-center justify-between h-full">
+        {/* Logo */}
+        <Link to={"/"}>
+          <div>
+            <img className="w-[40px]" src={Logo} alt="" />
+          </div>
+        </Link>
+        {/* cart */}
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="cursor-pointer flex relative "
+        >
+          <BsBag className="text-2xl" />
+          <div className="bg-red-500 absolute -right-2 -bottom-2 text-[12px] w-[18px] text-white rounded-full flex justify-center items-center">
+            {itemAmount}
+          </div>
+        </div>
       </div>
-    </div>
+    </header>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import ProductProvider from "./contexts/ProductContext";
 import SidebarProvider from "./contexts/SidebarContext";
+import CartProvider from "./contexts/CartContext";
 
 ReactDOM.render(
   <SidebarProvider>
-    <ProductProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </ProductProvider>
+    <CartProvider>
+      <ProductProvider>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ProductProvider>
+    </CartProvider>
   </SidebarProvider>,
   document.getElementById("root")
 );
